ed53a1985fc5ba458f9a0fe24176fea6a7e10ce2
Martin Bateman <mb@ziggy>
2014-11-26 07:40:42 +0000

    Initial commit

---
+++ btdriver.py
@@ -0,0 +1,97 @@
+import bluetooth
+import sys
+import uinput
+import struct
+import binascii
+#import bytearray
+from evdev import UInput, ecodes as e
+
+RIGHT_RIGHT = (1 << 0)
+RIGHT_TOP = (1 << 1)
+RIGHT_LEFT = (1 << 2)
+RIGHT_DOWN = (1 << 3)
+LEFT_RIGHT = (1 << 4)
+LEFT_TOP = (1 << 5)
+LEFT_LEFT = (1 << 6)
+LEFT_DOWN = (1 << 7)
+MIDDLE_RIGHT = (1 << 0)
+MIDDLE_LEFT = (1 << 1)
+
+ui = UInput ()
+
+# change you your bluetooth device address
+bd_addr = "98:D3:31:20:17:6E"
+
+port = 1
+sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+sock.connect((bd_addr, port))
+print ('Connected')
+
+lastNum1 = 0
+lastNum2 = 0
+
+while 1:
+  data1 = sock.recv(1)
+  data2 = sock.recv(1)
+  num1 = ord (data1)
+  num2 = ord (data2)
+
+  if ((num1 & RIGHT_RIGHT) and not(lastNum1 & RIGHT_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_U, 1)
+  if (not (num1 & RIGHT_RIGHT) and (lastNum1 & RIGHT_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_U, 0)
+
+  if ((num1 & RIGHT_TOP) and not (lastNum1 & RIGHT_TOP)):
+    ui.write (e.EV_KEY, e.KEY_I, 1)
+  if (not (num1 & RIGHT_TOP) and (lastNum1 & RIGHT_TOP)):
+    ui.write (e.EV_KEY, e.KEY_I, 0)
+
+  if ((num1 & RIGHT_LEFT) and not (lastNum1 & RIGHT_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_O, 1)
+  if (not (num1 & RIGHT_LEFT) and (lastNum1 & RIGHT_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_O, 0)
+
+  if ((num1 & RIGHT_DOWN) and not (lastNum1 & RIGHT_DOWN)):
+    ui.write (e.EV_KEY, e.KEY_P, 1)
+  if (not (num1 & RIGHT_DOWN) and (lastNum1 & RIGHT_DOWN)):
+    ui.write (e.EV_KEY, e.KEY_P, 0)
+
+
+  if ((num1 & LEFT_RIGHT) and not(lastNum1 & LEFT_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_RIGHT, 1)
+  if (not (num1 & LEFT_RIGHT) and (lastNum1 & LEFT_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_RIGHT, 0)
+
+  if ((num1 & LEFT_TOP) and not (lastNum1 & LEFT_TOP)):
+    ui.write (e.EV_KEY, e.KEY_UP, 1)
+  if (not (num1 & LEFT_TOP) and (lastNum1 & LEFT_TOP)):
+    ui.write (e.EV_KEY, e.KEY_UP, 0)
+
+  if ((num1 & LEFT_LEFT) and not (lastNum1 & LEFT_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_LEFT, 1)
+  if (not (num1 & LEFT_LEFT) and (lastNum1 & LEFT_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_LEFT, 0)
+
+  if ((num1 & LEFT_DOWN) and not (lastNum1 & LEFT_DOWN)):
+    ui.write (e.EV_KEY, e.KEY_DOWN, 1)
+  if (not (num1 & LEFT_DOWN) and (lastNum1 & LEFT_DOWN)):
+    ui.write (e.EV_KEY, e.KEY_DOWN, 0)
+
+
+  if ((num2 & MIDDLE_RIGHT) and not (lastNum2 & MIDDLE_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_J, 1)
+  if (not (num2 & MIDDLE_RIGHT) and (lastNum2 & MIDDLE_RIGHT)):
+    ui.write (e.EV_KEY, e.KEY_J, 0)
+
+  if ((num2 & MIDDLE_LEFT) and not (lastNum2 & MIDDLE_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_K, 1)
+  if (not (num2 & MIDDLE_LEFT) and (lastNum2 & MIDDLE_LEFT)):
+    ui.write (e.EV_KEY, e.KEY_K, 0)
+
+
+  ui.syn ()
+  lastNum1 = num1
+  lastNum2 = num2
+
+
+sock.close()
---
+++ gamepad.ino
@@ -0,0 +1,171 @@
+#define RIGHT_RIGHT_PIN A1
+#define RIGHT_TOP_PIN A0
+#define RIGHT_LEFT_PIN 8
+#define RIGHT_DOWN_PIN 9
+
+#define START_PIN 5
+#define SELECT_PIN 4
+
+#define LEFT_RIGHT_PIN 7
+#define LEFT_TOP_PIN A2
+#define LEFT_LEFT_PIN 6
+#define LEFT_DOWN_PIN A3
+
+#define RIGHT_RIGHT 1
+#define RIGHT_TOP 2
+#define RIGHT_LEFT 3
+#define RIGHT_DOWN 4
+
+#define LEFT_RIGHT 5
+#define LEFT_TOP 6
+#define LEFT_LEFT 7
+#define LEFT_DOWN 8
+
+#define START 9
+#define SELECT 10
+
+#define DEBOUNCE 50
+
+
+long lastButtonTime[16];
+
+int buttonState = 0;
+int lastButtonState = 0;
+
+int lastSent = 0;
+
+void setup() {
+    
+//right 4
+    pinMode(RIGHT_RIGHT_PIN, INPUT_PULLUP);     
+    pinMode(RIGHT_TOP_PIN, INPUT_PULLUP);
+    pinMode(RIGHT_LEFT_PIN, INPUT_PULLUP);     
+    pinMode(RIGHT_DOWN_PIN, INPUT_PULLUP);
+
+// middle 2
+    pinMode(START_PIN, INPUT_PULLUP);     
+    pinMode(SELECT_PIN, INPUT_PULLUP);
+
+//left 4
+    pinMode(LEFT_RIGHT_PIN, INPUT_PULLUP);     
+    pinMode(LEFT_TOP_PIN, INPUT_PULLUP);
+    pinMode(LEFT_LEFT_PIN, INPUT_PULLUP);     
+    pinMode(LEFT_DOWN_PIN, INPUT_PULLUP);
+
+    Serial.begin(9600);
+}
+
+void printButtonState (int state){
+  for (int i = 0; i < sizeof(state) * 8; i ++){
+    if (state & (1 << i)) {
+      Serial.print ("1");
+    } else {
+      Serial.print ("0");
+    }
+  }
+  Serial.println ("");
+}
+
+void setButtonOn (int BUTTON, long time) {
+
+  if (((lastButtonState & (1<<(BUTTON-1))) != (1<<(BUTTON - 1)))) {
+    lastButtonTime[BUTTON] = time;
+  }
+  if ((time - lastButtonTime[BUTTON]) > DEBOUNCE) {
+    if (((buttonState & (1<<(BUTTON-1))) != (1 << (BUTTON - 1)))){
+      buttonState = buttonState | (1 << (BUTTON - 1));
+    }
+  }
+  
+  lastButtonState = lastButtonState | (1 << (BUTTON - 1));
+}
+
+void setButtonOff (int BUTTON, long time) {
+  
+  if (((lastButtonState & (1<<(BUTTON-1))) !=  !(1<<(BUTTON - 1)))) {
+        lastButtonTime[BUTTON] = time;
+  }
+  
+  if ((time - lastButtonTime[BUTTON]) > DEBOUNCE) {
+    if (((lastButtonState & (1<<(BUTTON-1))) != (1 << (BUTTON - 1)))){
+       buttonState = buttonState & ~(1 << (BUTTON - 1));
+    }
+  }
+  
+  lastButtonState = lastButtonState & ~(1 << (BUTTON - 1));
+
+}
+
+void getButtonStateToSend (int buttonState) {
+  int toSend = 0;
+  boolean changed = 0;
+  for (int i = 0; i < sizeof (buttonState) *8; i ++) {
+    if ((lastSent & (1 << i)) != (buttonState & (1 << i))) {
+      changed = 1;
+    }
+  }
+  if (changed == 1) {
+    toSend = buttonState;
+    printButtonState (toSend);
+    lastSent = toSend;
+  }
+}
+
+void loop(){
+  long time = millis ();
+  if (digitalRead (RIGHT_RIGHT_PIN) == LOW) {
+    setButtonOn (RIGHT_RIGHT, time);
+  } else {
+    setButtonOff (RIGHT_RIGHT, time);
+  }
+  if (digitalRead (RIGHT_TOP_PIN) == LOW) {
+    setButtonOn (RIGHT_TOP, time);
+  } else {
+    setButtonOff (RIGHT_TOP, time);
+  }
+  if (digitalRead (RIGHT_LEFT_PIN) == LOW) {
+    setButtonOn (RIGHT_LEFT, time);
+  } else {
+    setButtonOff (RIGHT_LEFT, time);
+  }
+  if (digitalRead (RIGHT_DOWN_PIN) == LOW) {
+    setButtonOn (RIGHT_DOWN, time);
+  } else {
+    setButtonOff (RIGHT_DOWN, time);
+  }
+
+
+  if (digitalRead(SELECT_PIN) == LOW) {
+    setButtonOn (SELECT, time);
+  } else {
+    setButtonOff (SELECT, time);
+  }
+  if (digitalRead (START_PIN) == LOW) {
+    setButtonOn (START, time);
+  } else {
+    setButtonOff (START, time);
+  }
+
+  if (digitalRead (LEFT_RIGHT_PIN) == LOW) {
+    setButtonOn (LEFT_RIGHT, time);
+  } else {
+    setButtonOff (LEFT_RIGHT, time);
+  }
+  if (digitalRead (LEFT_TOP_PIN) == LOW) {
+    setButtonOn (LEFT_TOP, time);
+  } else {
+    setButtonOff (LEFT_TOP, time);
+  }
+  if (digitalRead(LEFT_LEFT_PIN) == LOW) {
+    setButtonOn (LEFT_LEFT, time);
+  } else {
+    setButtonOff (LEFT_LEFT, time);
+  }
+  if (digitalRead (LEFT_DOWN_PIN) == LOW) {
+    setButtonOn (LEFT_DOWN, time);
+  } else {
+    setButtonOff (LEFT_DOWN, time);
+  }
+  getButtonStateToSend (buttonState);
+
+}
